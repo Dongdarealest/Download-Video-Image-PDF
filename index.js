e92619cdecafcdfff28785afbcc208a055771ae0
Dongdarealest <107338168+Dongdarealest@users.noreply.github.com>
2022-07-26 15:02:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
     <title>File Downloader</title>
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
+    <meta name="description" content="Download Image, Video, PDF">
+ </head>
   <body>
     <div class="wrapper">
       <header>
